e1f54846788ad560556b6a8d7ff547a48af5faea
apoloniaabramowicz <pola.abramowicz@onet.pl>
2024-11-22 19:10:36 +0100

    feat: add api configuration

---
+++ src/lib/api.ts
@@ -0,0 +1,18 @@
+import { API_URL } from "@/config/api";
+
+export async function fetchData<T>(endpoint: string, options?: RequestInit ): Promise<T> {
+    console.log(`${API_URL}/${endpoint}`)
+    const response = await fetch(`${API_URL}/${endpoint}`,{
+        ...options,
+        headers: {
+            "Content-Type": "application/json",
+            ...(options?.headers || {}),
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
+    return response.json();
+}
