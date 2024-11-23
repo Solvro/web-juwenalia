efa5229b6acd421a29e2e9edfe3f70f4d1d98942
zeolsem <zsolim@proton.me>
2024-11-23 21:21:07 +0100

    feat: add news demo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 
 const Navbar = () => {
-    return (
-        <div className="flex-col space-x-1.5 text-blue-800">
-            <Link href="/">Home</Link>
-            <Link href="/schedule">About</Link>
-            <Link href="/map">Map</Link>
-            <Link href="/postsFb">Fb</Link>
-            <Link href="/postsIg">Ig</Link>
-        </div>
-    )
-}
+  return (
+    <div className="flex-col space-x-1.5 text-blue-800">
+      <Link href="/">Home</Link>
+      <Link href="/schedule">About</Link>
+      <Link href="/map">Map</Link>
+      <Link href="/postsFb">Fb</Link>
+      <Link href="/postsIg">Ig</Link>
+      <Link href="/news">News</Link>
+    </div>
+  );
+};
 
-export { Navbar }
+export { Navbar };
