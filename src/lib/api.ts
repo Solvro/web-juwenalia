21178c639b4aff678c199d4d0859c0fcfe52faeb
apoloniaabramowicz <pola.abramowicz@onet.pl>
2024-11-26 14:08:15 +0100

    chore: add custom font Montserrat and prettier

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,18 +1,20 @@
 import { API_URL } from "@/config/api";
 
-export async function fetchData<T>(endpoint: string, options?: RequestInit ): Promise<T> {
+export async function fetchData<T>(
+  endpoint: string,
+  options?: RequestInit,
+): Promise<T> {
+  const response = await fetch(`${API_URL}/${endpoint}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...(options?.headers ?? {}),
+    },
+  });
 
-    const response = await fetch(`${API_URL}/${endpoint}`,{
-        ...options,
-        headers: {
-            "Content-Type": "application/json",
-            ...(options?.headers ?? {}),
-        }
-    });
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
 
-    if (!response.ok) {
-        throw new Error(response.statusText);
-    }
-
-    return response.json();
+  return response.json();
 }
