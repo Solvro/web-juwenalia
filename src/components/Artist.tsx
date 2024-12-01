deadf273ed6bef1bd2c3ff90096d5d408990e5d7
zeolsem <zsolim@proton.me>
2024-12-01 20:43:51 +0100

    fix: adjust ArtistDemo code to fit new model

--- src/components/Artist.tsx
+++ src/components/Artist.tsx
@@ -3,13 +3,12 @@ import Link from 'next/link';
 import { API_URL } from '@/config/api';
 import { ArtistProps } from '@/lib/types';
 
-const Artist = async ({ url, id, name, description, image }: ArtistProps) => {
+const Artist = async ({ id, name, description, image, instagramUrl, spotifyUrl, isPopular }: ArtistProps) => {
   return (
-    <Link href={url} target="_blank" rel="noopener noreferrer">
+    <div>
       <div key={id}>
         <h2 className={'font-bold'}>{name}</h2>
         <p className={'w-1/3'}>{description}</p>
-        {url}
         <Image
           className={'aspect-square object-cover'}
           src={`${API_URL}/assets/${image}`}
@@ -17,8 +16,21 @@ const Artist = async ({ url, id, name, description, image }: ArtistProps) => {
           width={200}
           height={200}
         />
+        { instagramUrl ?
+        <Link href={instagramUrl} target="_black" rel="noopener noreferrer">
+          <span className='p-4 m-4'>Instagram</span>
+        </Link>
+        : <h1>Ten artysta nie podal instagrama</h1>
+        }
+        { spotifyUrl ?
+        <Link href={spotifyUrl} target="_black" rel="noopener noreferrer">
+          <span className='p-4 m-4'>Spotify</span>
+        </Link>
+        : <h1>Ten artysta nie podal spotifaja</h1>
+        }
+        { isPopular && <h1>Hot</h1> }
       </div>
-    </Link>
+    </div>
   );
 };
 
