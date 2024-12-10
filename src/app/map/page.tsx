94299dc401ded9245281d3a2e19ff7708d6a17b3
Dawid Kowal <dawidkowal04@gmail.com>
2024-12-10 19:08:55 +0100

    fix: fix html table structure and dynamic import

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -6,7 +6,7 @@ import { InteractiveMap } from "@/components/InteractiveMap";
 
 // import dynamic from "next/dynamic";
 
-// const DynamicInteractiveMap = dynamic(() => import("@/components/InteractiveMap"), {
+// const DynamicInteractiveMap = dynamic(() => import("@/components/InteractiveMap").then(mod => mod.InteractiveMap), {
 //   ssr: false,
 // });
 
