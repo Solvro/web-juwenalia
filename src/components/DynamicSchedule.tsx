c017eb7b084801aaf5ab158b965d2a638e617846
apoloniaabramowicz <pola.abramowicz@onet.pl>
2024-12-14 14:02:40 +0100

    feat: add event name and is on

---
+++ src/components/DynamicSchedule.tsx
@@ -0,0 +1,78 @@
+
+"use client";
+
+import { DayProps } from "@/lib/types";
+import { format, isWithinInterval } from "date-fns";
+
+interface Props {
+  daysList: DayProps[];
+}
+
+const DynamicSchedule: React.FunctionComponent<Props> = ({ daysList }) => {
+
+  const days = daysList.map((day) => ({
+    ...day,
+    date: new Date(day.date)
+  }));
+
+  const combineDateAndTime = (date: Date, time: string): Date => {
+    const [hours, minutes] = time.split(":").map(Number);
+    const combinedDate = new Date(date);
+    combinedDate.setHours(hours, minutes, 0, 0); // Set hours, minutes, and reset seconds and milliseconds
+    return combinedDate;
+  };
+
+  const isNowOn = (start: Date, end: Date) => {
+    const currentDate = new Date();
+    return isWithinInterval(currentDate, { start, end });
+  };
+
+  const weekDays = [
+    "Niedziela",
+    "Poniedzialek",
+    "Wtorek",
+    "Środa",
+    "Czwartek",
+    "Piątek",
+    "Sobota"
+  ];
+  return (
+    <div className="p-4">
+      <h1 className={"text-xl font-extrabold mb-6"}>Schedule</h1>
+      {days.map((day) => (
+        <div key={day.id} className={"border-b-2 border-gray-300 pb-4 mb-6"}>
+          <h2 className={"font-bold text-lg"}>{weekDays[day.date.getDay()]}</h2>
+          <h3 className={"text-gray-600"}>{format(day.date, "dd.MM.yyyy")}</h3>
+          <h2 className={"mt-4 text-md font-semibold"}>Events:</h2>
+          {day.events.map((event) => {
+            const eventStart = combineDateAndTime(day.date, event.start_time);
+            const eventEnd = combineDateAndTime(day.date, event.end_time);
+            const isOn = isNowOn(eventStart, eventEnd);
+            return (
+              <div key={event.id} className={"ml-4 mt-2"}>
+                {isOn &&
+                  <p className={"text-white-600 bg-gradient-secondary"}>Is on</p>
+                }
+                <h2>{`${event.start_time}-${event.end_time}`}</h2>
+                <h3>{event.location.name}</h3>
+                {event.artists.length !== 0 ? (
+                  <div className={"ml-4"}>
+                    {event.artists.map((artist) => (
+                      <div key={artist.id} className={"text-sm text-gray-800"}>
+                        {artist.artists_id.name}
+                      </div>
+                    ))}
+                  </div>
+                ):(
+                  <div className={"ml-4"}>{event.name}</div>
+                )}
+
+              </div>);
+          })}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export { DynamicSchedule };
