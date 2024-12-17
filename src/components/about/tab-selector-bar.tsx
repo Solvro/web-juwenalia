5ee2b7edfc381961da143e4e01c44361897ba362
Konrad (PC) <konrad@guzek.uk>
2024-12-17 23:46:54 +0100

    feat: shiny animated tab selector

--- src/components/about/tab-selector-bar.tsx
+++ src/components/about/tab-selector-bar.tsx
@@ -1,22 +1,30 @@
+import { useEffect, useRef, useState } from "react";
+import type { MouseEvent, RefObject } from "react";
+
 import { Button } from "../ui/button";
 
 function TabSelector<T>({
   text,
   idx,
   selectedIdx,
-  setSelectedIdx,
+  onClick,
+  refObject,
 }: {
   text: string;
   idx: T;
   selectedIdx: T;
-  setSelectedIdx: React.Dispatch<React.SetStateAction<T>>;
+  onClick: (event: MouseEvent<HTMLButtonElement>, index: T) => void;
+  refObject?: RefObject<HTMLButtonElement>;
 }) {
-  const selected = idx === selectedIdx;
+  const isSelected = idx === selectedIdx;
   return (
     <li>
       <Button
-        className={`rounded-full text-xs text-black ${selected ? "bg-gradient-main text-white" : "bg-transparent"}`}
-        onClick={() => setSelectedIdx(idx)}
+        ref={refObject}
+        className={`rounded-full bg-transparent text-xs ${isSelected ? "text-white" : "text-black"} transition-colors duration-300`}
+        onClick={(event) => {
+          onClick(event, idx);
+        }}
       >
         {text}
       </Button>
@@ -33,15 +41,40 @@ export function TabSelectorBar<T extends readonly any[]>({
   selectedIdx: keyof T;
   setSelectedIdx: React.Dispatch<React.SetStateAction<keyof T>>;
 }) {
+  const [markerWidth, setMarkerWidth] = useState(0);
+  const [markerOffset, setMarkerOffset] = useState(0);
+  const markerRef = useRef<HTMLDivElement>(null);
+  const initialSelectedRef = useRef<HTMLButtonElement>(null);
+
+  function onSwitchTab(event: MouseEvent<HTMLButtonElement>, index: keyof T) {
+    setSelectedIdx(index);
+    setMarkerWidth(event.currentTarget.offsetWidth);
+    setMarkerOffset(event.currentTarget.offsetLeft);
+  }
+
+  useEffect(() => {
+    // Set initial marker width based on initial selected button
+    if (initialSelectedRef.current != null) {
+      setMarkerWidth(initialSelectedRef.current.offsetWidth);
+      setMarkerOffset(initialSelectedRef.current.offsetLeft);
+    }
+  }, [initialSelectedRef]);
+
   return (
-    <ul className="flex flex-wrap justify-center gap-1 sm:justify-start">
+    <ul className="relative flex flex-wrap justify-center gap-1 sm:justify-start">
+      <div
+        className="absolute bottom-0 -z-10 h-full rounded-full bg-gradient-main text-white transition-all duration-300"
+        ref={markerRef}
+        style={{ width: markerWidth, left: `${markerOffset.toString()}px` }}
+      ></div>
       {options.map((option, index) => (
         <TabSelector
-          key={idx}
-          idx={idx as keyof T}
-          text={option}
+          key={option}
+          idx={index as keyof T}
           selectedIdx={selectedIdx}
-          setSelectedIdx={setSelectedIdx}
+          text={option}
+          onClick={onSwitchTab}
+          refObject={index === selectedIdx ? initialSelectedRef : undefined}
         />
       ))}
     </ul>
