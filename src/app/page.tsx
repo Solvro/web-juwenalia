d1264d696f769d202ade7e4ef0fefce74211ed88
Tymon <jedryczkatymon@gmail.com>
2024-12-23 20:52:02 +0100

    feat: add generic carousel component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { AboutUs } from "@/components/about-us/about-us-section";
 import { ArtistsCarousel } from "@/components/artists-carousel";
 import { BuyTicketCta } from "@/components/buy-ticket-cta";
 import { Countdown } from "@/components/countdown";
+import { Carousel } from "@/components/generic-carousel-component";
 import { LatestNews } from "@/components/latest-news/latest-news-section";
 import { Schedule } from "@/components/schedule/schedule";
 
