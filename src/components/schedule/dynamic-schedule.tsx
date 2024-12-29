b2bd8828d3bb5a9145a8ed2b63f9755885f54bbf
apoloniaabramowicz <pola.abramowicz@onet.pl>
2024-12-29 23:22:43 +0100

    feat: add location element style

--- src/components/schedule/dynamic-schedule.tsx
+++ src/components/schedule/dynamic-schedule.tsx
@@ -49,24 +49,29 @@ function DynamicSchedule({ daysList }: Props): React.ReactElement {
                 {isOn ? (
                   <p className="text-white-600 bg-gradient-secondary">Is on</p>
                 ) : null}
-                <div className="flex flex-col justify-between px-10 py-[35px] sm:flex-row sm:items-center">
-                  {event.artists.length > 0 ? (
-                    <div>
-                      {event.artists.map((artist) => (
-                        <div
-                          key={artist.id}
-                          className="py-1 text-xl text-gray-800 lg:text-2xl xl:text-3xl"
-                        >
-                          {artist.artists_id.name}
-                        </div>
-                      ))}
+                <div className="flex flex-col justify-between px-4 sm:flex-row sm:items-center sm:py-[15px] md:px-10 md:py-[35px]">
+                  <div className="flex flex-col">
+                    {event.artists.length > 0 ? (
+                      <div>
+                        {event.artists.map((artist) => (
+                          <div
+                            key={artist.id}
+                            className="text-l py-1 text-gray-800 sm:text-xl md:text-2xl xl:text-3xl"
+                          >
+                            {artist.artists_id.name}
+                          </div>
+                        ))}
+                      </div>
+                    ) : (
+                      <div className="text-l text-gray-800 md:text-2xl xl:text-3xl">
+                        {event.name}
+                      </div>
+                    )}
+                    <div className="md:text-l text-xs font-light sm:text-sm xl:text-xl">
+                      {event.location.name}
                     </div>
-                  ) : (
-                    <div className="text-xl text-gray-800 lg:text-2xl xl:text-3xl">
-                      {event.name}
-                    </div>
-                  )}
-                  <div className="mt-4 flex-none text-left text-xl font-semibold sm:mt-0 sm:text-right lg:text-2xl xl:text-3xl">
+                  </div>
+                  <div className="text-l mt-4 flex-none text-left font-semibold sm:mr-32 sm:mt-0 sm:text-right sm:text-xl md:text-2xl xl:text-3xl">
                     <h2>{`${event.start_time.split(":").slice(0, 2).join(":")}-${event.end_time.split(":").slice(0, 2).join(":")}`}</h2>
                   </div>
                 </div>
