129ef402a10ee91326f60ca5878d7d961028e86b
apoloniaabramowicz <pola.abramowicz@onet.pl>
2024-12-29 23:22:43 +0100

    feat: add location element style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { AboutUs } from "@/components/about-us/about-us-section";
 import { Countdown } from "@/components/countdown";
 import { LatestNews } from "@/components/latest-news/latest-news-section";
-import { Schedule } from "@/components/schedule";
+import { Schedule } from "@/components/schedule/schedule";
 
 export default function Home() {
   return (
