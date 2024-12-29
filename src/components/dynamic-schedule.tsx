142b6cca4077b38a66dd0cc51d007ebd85147b18
apoloniaabramowicz <pola.abramowicz@onet.pl>
2024-12-29 22:35:57 +0100

    feat: add day element style
    # Conflicts:
    #	src/components/horizontal-rule.tsx

--- src/components/dynamic-schedule.tsx
+++ src/components/dynamic-schedule.tsx
@@ -3,6 +3,8 @@
 import { format, isWithinInterval } from "date-fns";
 import { pl } from "date-fns/locale";
 
+import { HomepageHeader } from "@/components/homepage-header";
+import { HorizontalRule } from "@/components/horizontal-rule";
 import type { DayProps } from "@/lib/types";
 
 interface Props {
@@ -27,26 +29,14 @@ function DynamicSchedule({ daysList }: Props): React.ReactElement {
     date: new Date(day.date),
   }));
 
-  const weekDays = [
-    "Niedziela",
-    "Poniedzialek",
-    "Wtorek",
-    "Środa",
-    "Czwartek",
-    "Piątek",
-    "Sobota",
-  ];
-
   return (
     <div>
       {/*<h1 className={"text-xl font-extrabold mb-6"}>Schedule</h1>*/}
       {days.map((day) => (
-        <div key={day.id}>
-          <div className="inline-flex items-center justify-end rounded-br-[100px] rounded-tr-[100px] bg-gradient-main py-3 pl-[10px] pr-[30px] md:py-[40px] md:pl-[65.10px] md:pr-[75px]">
-            <div className="font-['Montserrat'] text-xl font-extrabold text-white md:text-[52px]">
-              {format(day.date, "d MMMM (EEEE)", { locale: pl })}
-            </div>
-          </div>
+        <div key={day.id} className="mt-[100px]">
+          <HomepageHeader>
+            {format(day.date, "d MMMM (EEEE)", { locale: pl })}
+          </HomepageHeader>
           {/*<table>*/}
           {/*  <tbody>*/}
           {day.events.map((event) => {
@@ -54,68 +44,39 @@ function DynamicSchedule({ daysList }: Props): React.ReactElement {
             const eventEnd = combineDateAndTime(day.date, event.end_time);
             const isOn = isNowOn(eventStart, eventEnd);
             return (
-              <div key={event.id} className={"ml-4 mt-2"}>
-                {isOn && (
-                  <p className={"text-white-600 bg-gradient-secondary"}>
-                    Is on
-                  </p>
-                )}
-                <h2>{`${event.start_time}-${event.end_time}`}</h2>
-                <h3>{event.location.name}</h3>
-                {event.artists.length > 0 ? (
-                  <div className={"ml-4"}>
-                    {event.artists.map((artist) => (
-                      <div key={artist.id} className={"text-sm text-gray-800"}>
-                        {artist.artists_id.name}
-                      </div>
-                    ))}
+              <div key={event.id}>
+                <HorizontalRule />
+                {isOn ? (
+                  <p className="text-white-600 bg-gradient-secondary">Is on</p>
+                ) : null}
+                <div className="flex flex-col justify-between px-10 py-[35px] sm:flex-row sm:items-center">
+                  {event.artists.length > 0 ? (
+                    <div>
+                      {event.artists.map((artist) => (
+                        <div
+                          key={artist.id}
+                          className="py-1 text-xl text-gray-800 lg:text-2xl xl:text-3xl"
+                        >
+                          {artist.artists_id.name}
+                        </div>
+                      ))}
+                    </div>
+                  ) : (
+                    <div className="text-xl text-gray-800 lg:text-2xl xl:text-3xl">
+                      {event.name}
+                    </div>
+                  )}
+                  <div className="mt-4 flex-none text-left text-xl font-semibold sm:mt-0 sm:text-right lg:text-2xl xl:text-3xl">
+                    <h2>{`${event.start_time.split(":").slice(0, 2).join(":")}-${event.end_time.split(":").slice(0, 2).join(":")}`}</h2>
                   </div>
-                ) : (
-                  <div className={"ml-4"}>{event.name}</div>
-                )}
+                </div>
               </div>
             );
           })}
+          <HorizontalRule />
         </div>
       ))}
     </div>
-
-    //         </tbody>
-    //       </table>
-    //     </div>
-    //
-    //     // <div key={day.id} className={"border-b-2 border-gray-300 pb-4 mb-6"}>
-    //     //   <h2 className={"font-bold text-lg"}>{weekDays[day.date.getDay()]}</h2>
-    //     //   <h3 className={"text-gray-600"}>{format(day.date, "dd.MM.yyyy")}</h3>
-    //     //   <h2 className={"mt-4 text-md font-semibold"}>Events:</h2>
-    //       {day.events.map((event) => {
-    //         const eventStart = combineDateAndTime(day.date, event.start_time);
-    //         const eventEnd = combineDateAndTime(day.date, event.end_time);
-    //         const isOn = isNowOn(eventStart, eventEnd);
-    //         return (
-    //           <div key={event.id} className={"ml-4 mt-2"}>
-    //             {isOn &&
-    //               <p className={"text-white-600 bg-gradient-secondary"}>Is on</p>
-    //             }
-    //             <h2>{`${event.start_time}-${event.end_time}`}</h2>
-    //             <h3>{event.location.name}</h3>
-    //             {event.artists.length !== 0 ? (
-    //               <div className={"ml-4"}>
-    //                 {event.artists.map((artist) => (
-    //                   <div key={artist.id} className={"text-sm text-gray-800"}>
-    //                     {artist.artists_id.name}
-    //                   </div>
-    //                 ))}
-    //               </div>
-    //             ):(
-    //               <div className={"ml-4"}>{event.name}</div>
-    //             )}
-    //
-    //           </div>);
-    //       })}
-    //     </div>
-    //   ))}
-    // </div>
   );
 }
 
