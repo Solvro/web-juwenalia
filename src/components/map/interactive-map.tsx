5fd0f565f579b557a1a5084a3623fbd41a94c247
Dawid Kowal <dawidkowal04@gmail.com>
2025-01-03 11:44:55 +0100

    fix: resolve map dragging and legend styling issues

--- src/components/map/interactive-map.tsx
+++ src/components/map/interactive-map.tsx
@@ -1,70 +1,15 @@
 "use client";
 
-// @ts-ignore
 import { Marker } from "@adamscybot/react-leaflet-component-marker";
 import "leaflet/dist/leaflet.css";
 import { MapPin } from "lucide-react";
 import { useEffect } from "react";
-import { MapContainer, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Popup, TileLayer, useMap } from "react-leaflet";
 
 import { wrLegendItems } from "@/config/wr-legend-items";
-import { LeafletElement } from "@/lib/types";
+import type { LeafletElement } from "@/lib/types";
 
 import { WrMapLegend } from "./wr-map-legend";
-
-const trams = [
-  {
-    lineNumber: 0,
-    terminus: "Zoo, Dworzec Główny",
-  },
-  {
-    lineNumber: 2,
-    terminus: "Krzyki, Biskupin",
-  },
-  {
-    lineNumber: 4,
-    terminus: "Biskupin, Oporów",
-  },
-  {
-    lineNumber: 10,
-    terminus: "Leśnica, Biskupin",
-  },
-  {
-    lineNumber: 19,
-    terminus: "Zoo, Kozanów (Dokerska)",
-  },
-  {
-    lineNumber: 78,
-    terminus: "Tarnogaj, Biskupin",
-  },
-];
-
-const busses = [
-  {
-    lineNumber: 145,
-    terminus: "Iwiny - Rondo, Sępolno",
-  },
-  {
-    lineNumber: 146,
-    terminus: "Bartoszowice, Gaj - Pętla",
-  },
-  {
-    lineNumber: 253,
-    terminus: "Leśnica, Sępolno",
-  },
-  {
-    lineNumber: 255,
-    terminus: "Iwiny - Rondo, Sępolno",
-  },
-  {
-    lineNumber: 315,
-    terminus: "Swojczyce, Reja",
-  },
-  {
-    lineNumber: 345,
-    terminus: "Stadion Olimpijski (Pętla po mieście)",
-  },
-];
 
 export function InteractiveMap() {
   useEffect(() => {
@@ -77,17 +22,26 @@ export function InteractiveMap() {
     };
   }, []);
 
+  function MapInitializer() {
+    const map = useMap();
+    useEffect(() => {
+      map.dragging.enable();
+    }, [map]);
+    return null;
+  }
+
   return (
     <div>
       <h1 className="mt-8 pl-5 text-4xl font-semibold">Mapa Wydarzenia</h1>
       <hr className="mb-16 mt-5" />
       <MapContainer
-        center={[51.106_972_989_320_404, 17.077_329_824_567_546]}
+        center={[51.106_972, 17.077_329]}
         zoom={16}
         scrollWheelZoom={true}
-        dragging={true}
-        className="mx-auto h-[75vh] w-[90%] touch-auto rounded-3xl object-cover"
+        dragging={false}
+        className="relative z-0 mx-auto h-[75vh] w-[90%] touch-auto rounded-3xl object-cover"
       >
+        <MapInitializer />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copycenter">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -111,62 +65,6 @@ export function InteractiveMap() {
         Legenda
       </h2>
       <WrMapLegend className="my-10 grid" items={wrLegendItems} />
-      {/* <Marker position={[51.107_093_550_006_816, 17.073_462_384_146_13]}>
-          <Popup>
-            <table className="gap-2">
-              <thead>
-                <tr>
-                  <th>
-                    Linia <br />
-                    Tramwajowa
-                  </th>
-                  <th>
-                    Przystanki <br />
-                    Końcowe
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {trams.map((tram) => (
-                  <tr key={tram.lineNumber}>
-                    <td className="w-fit text-center font-bold">
-                      {tram.lineNumber}
-                    </td>
-                    <td>{tram.terminus}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </Popup>
-        </Marker>
-        <Marker position={[51.107_451_696_565_01, 17.072_652_788_986_44]}>
-          <Popup>
-            <table className="gap-2">
-              <thead>
-                <tr>
-                  <th>
-                    Linia <br />
-                    Autobusowa
-                  </th>
-                  <th>
-                    Przystanki <br />
-                    Końcowe
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {busses.map((bus) => (
-                  <tr key={bus.lineNumber}>
-                    <td className="w-fit text-center font-bold">
-                      {bus.lineNumber}
-                    </td>
-                    <td>{bus.terminus}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </Popup>
-        </Marker> */}
     </div>
   );
 }
--- src/config/wr-legend-items.tsx
+++ src/config/wr-legend-items.tsx
@@ -1,16 +1,22 @@
-import { WrLegendItem } from "@/lib/types";
+import type { WrLegendItem } from "@/lib/types";
 
 export const wrLegendItems: WrLegendItem[] = [
   {
     name: "Hala Stulecia",
     description: "Główny teren festiwalu",
     color: "#CD2E32",
-    coordinates: [51.10692, 17.07735],
+    coordinates: [51.106_921, 17.077_351],
   },
   {
     name: "Śniadanie z Rektorem",
     description: "Śniadanie z Rektorem Politechniki Wrocławskiej",
     color: "#000000",
-    coordinates: [51.107372, 17.061764],
+    coordinates: [51.107_372, 17.061_764],
+  },
+  {
+    name: "Jakieś inne wydarzenie",
+    description: "Na pewno ma bardzo ciekawy opis",
+    color: "#3099ff",
+    coordinates: [51.105_037, 17.086_041],
   },
 ];
