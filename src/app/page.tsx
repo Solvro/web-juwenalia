15df7bc54f4ddff8053db52e52499729f504be42
Adriskk <65545676+Adriskk@users.noreply.github.com>
2025-01-07 22:23:08 +0100

    feat: create buy ticket cta on homepage, small component fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { AboutUs } from "@/components/about-us/about-us-section";
+import { BuyTicketCta } from "@/components/buy-ticket-cta";
 import { Countdown } from "@/components/countdown";
 import { LatestNews } from "@/components/latest-news/latest-news-section";
 import { Schedule } from "@/components/schedule/schedule";
@@ -9,6 +10,7 @@ export default function Home() {
       <div>This is the main page</div>
       <Countdown />
       <Schedule />
+      <BuyTicketCta />
       <LatestNews />
       <AboutUs />
     </>
