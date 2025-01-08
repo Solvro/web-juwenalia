c0ece9bfb95a27ec45e227f6c278f3ae3ea5b4aa
apoloniaabramowicz <pola.abramowicz@onet.pl>
2025-01-08 21:32:00 +0100

    chore: separate day component

---
+++ src/components/schedule/day.tsx
@@ -0,0 +1,48 @@
+import { HorizontalRule } from "@/components/horizontal-rule";
+import type { EventProps } from "@/lib/types";
+
+interface Props {
+  event: EventProps;
+  isOn: boolean;
+}
+
+export function Day({ event, isOn }: Props) {
+  return (
+    <div>
+      <HorizontalRule />
+      <div className="flex flex-col justify-between px-4 sm:flex-row sm:items-center sm:py-6 md:px-10 md:py-[35px]">
+        <div className="flex flex-col">
+          {isOn ? (
+            <div className="my-1 flex max-w-28 items-center justify-center rounded-md bg-gradient-main px-2.5 py-[5px]">
+              <div className="text-[10px] font-black text-white">
+                WŁAŚNIE TRWA
+              </div>
+            </div>
+          ) : null}
+          {event.artists.length > 0 ? (
+            <div>
+              {event.artists.map((artist) => (
+                <div
+                  key={artist.id}
+                  className="text-l py-1 text-gray-800 sm:text-xl md:text-2xl xl:text-3xl"
+                >
+                  {artist.artists_id.name}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="text-l text-gray-800 md:text-2xl xl:text-3xl">
+              {event.name}
+            </div>
+          )}
+          <div className="md:text-l text-xs font-light sm:text-sm xl:text-xl">
+            {event.location.name}
+          </div>
+        </div>
+        <div className="text-l mt-4 flex-none text-left font-semibold sm:mr-32 sm:mt-0 sm:text-right sm:text-xl md:text-2xl xl:text-3xl">
+          <h2>{`${event.start_time.split(":").slice(0, 2).join(":")}-${event.end_time.split(":").slice(0, 2).join(":")}`}</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
