410489a37d726a690b7a70442310b7270ef3a341
Dawid Kowal <dawidkowal04@gmail.com>
2025-01-14 17:53:33 +0100

    fix: legend spacing

--- src/components/map/wr-map-legend.tsx
+++ src/components/map/wr-map-legend.tsx
@@ -12,14 +12,16 @@ export function WrMapLegend({ items, className }: LegendProps) {
   return (
     <div
       className={cn(
-        "mx-auto grid w-[90%] grid-cols-2 gap-y-2 sm:grid-cols-[repeat(3,minmax(0,1fr))] md:w-max",
+        "mx-auto grid w-[90%] grid-cols-2 gap-x-5 gap-y-2 sm:grid-cols-[repeat(3,minmax(0,20vw))] md:w-max",
         className,
       )}
     >
       {items.map((item) => (
         <div key={item.name} className="flex items-center">
           <MapPin className="mr-2 h-8 w-8" color={item.color} />
-          <p className="w-fit text-[3vw] sm:text-base">{item.name}</p>
+          <p className="w-[75%] text-wrap text-[3vw] sm:text-base">
+            {item.name}
+          </p>
         </div>
       ))}
     </div>
