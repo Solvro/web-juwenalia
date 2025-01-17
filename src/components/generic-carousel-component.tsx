eb26ef36aaad5ea6139482fafac9156e1534ae78
Tymon <jedryczkatymon@gmail.com>
2025-01-17 13:27:27 +0100

    chore: commit to fix

--- src/components/generic-carousel-component.tsx
+++ src/components/generic-carousel-component.tsx
@@ -1,13 +1,17 @@
-"use client";
-
-import React from "react";
+import dynamic from "next/dynamic";
 import type { Settings } from "react-slick";
-import Slider from "react-slick";
+
+import { Artist } from "@/components/artist";
+import { getArtists } from "@/lib/artists";
 
 import "../app/slick-theme.css";
 import "../app/slick.css";
 
-function Carousel() {
+const Slider = dynamic(async () => import("react-slick"), { ssr: true });
+
+async function Carousel() {
+  const artists = await getArtists();
+
   const settings: Settings = {
     dots: true,
     infinite: false,
@@ -16,38 +20,21 @@ function Carousel() {
     slidesToScroll: 3,
     initialSlide: 0,
     responsive: [
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1.7,
-          slidesToScroll: 1.7,
-        },
-      },
-      {
-        breakpoint: 400,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
+      { breakpoint: 800, settings: { slidesToShow: 2, slidesToScroll: 2 } },
+      { breakpoint: 600, settings: { slidesToShow: 1.5, slidesToScroll: 1.5 } },
+      { breakpoint: 500, settings: { slidesToShow: 1.4, slidesToScroll: 1.4 } },
+      { breakpoint: 400, settings: { slidesToShow: 1, slidesToScroll: 1 } },
     ],
   };
 
   return (
-    <div className="slider-container mx-auto my-20 flex h-fit w-4/5 flex-col justify-center">
+    <div className="slider-container mx-auto my-20 flex h-fit w-11/12 flex-col justify-center">
       <Slider {...settings}>
-        <div className="h-96 border-4 border-solid border-red-900">
-          <h3>1</h3>
-        </div>
-        <div className="h-96 border-4 border-solid border-red-900">
-          <h3>2</h3>
-        </div>
-        <div className="h-96 border-4 border-solid border-red-900">
-          <h3>3</h3>
-        </div>
-        <div className="h-96 border-4 border-solid border-red-900">
-          <h3>4</h3>
-        </div>
+        {artists.map((artist) => (
+          <div key={artist.id} className="p-4">
+            <Artist {...artist} />
+          </div>
+        ))}
       </Slider>
     </div>
   );
