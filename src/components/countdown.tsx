7c24873d73f88a0fa5b9571ff014d4ee8daafbf9
apoloniaabramowicz <pola.abramowicz@onet.pl>
2025-01-28 16:39:35 +0100

    chore: change font sizes

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -16,7 +16,7 @@ const eventStartDate: Date = new Date(2025, 4, 21, 16); //It's the date from pwr
 const eventEndDate: Date = new Date(2025, 4, 23, 2);
 function TextsForTimer({ children }: { children: ReactNode }) {
   return (
-    <div className="text-[2.3vh] font-medium sm:mb-[-20px] sm:text-[20px] lg:text-[24px] xl:text-[28px]">
+    <div className="mt-2.5 text-base font-medium sm:mt-0 sm:text-[20px] lg:text-[24px] xl:text-[28px]">
       {children}
     </div>
   );
@@ -69,8 +69,8 @@ function Countdown() {
     seconds: forDuration.seconds ?? 0,
   };
   return (
-    <div className="relative top-[-50px] z-[-10] h-[90vh] w-full flex-shrink-0 rounded-b-[40px] bg-[url('/images/background_homepage.png')] from-[rgba(0,0,0,0.20)] via-[rgba(0,0,0,0.40)] to-transparent bg-cover bg-center bg-no-repeat sm:h-[85vh] sm:w-full sm:rounded-b-[60px] sm:bg-[url('/images/background_homepage.png')] sm:from-[rgba(0,0,0,0.40)] sm:via-[rgba(0,0,0,0.20)] sm:to-transparent lg:h-[90vh] xl:h-[95vh]">
-      <div className="absolute left-5 top-10 text-[8vh] font-extrabold leading-[7vh] text-[#FFF] sm:left-1/2 sm:top-1/2 sm:-translate-x-1/2 sm:-translate-y-[50%] sm:transform sm:text-[76px] sm:leading-[80px] lg:text-[112px] lg:leading-[96px] xl:text-[156px] xl:leading-[110px]">
+    <div className="relative top-[-80px] z-[-10] h-[77vh] w-full flex-shrink-0 rounded-b-[40px] bg-[url('/images/background_homepage.png')] bg-cover bg-center bg-no-repeat before:absolute before:inset-0 before:rounded-b-[40px] before:bg-black before:opacity-30 sm:top-[-50px] sm:h-[90vh] sm:w-full sm:rounded-b-[60px] sm:before:sm:rounded-b-[60px] lg:h-[90vh] xl:h-[95vh]">
+      <div className="absolute left-5 top-10 mt-20 text-[8vh] font-extrabold leading-[7vh] text-[#FFF] sm:left-1/2 sm:top-1/2 sm:-translate-x-1/2 sm:-translate-y-[50%] sm:transform sm:text-[76px] sm:leading-[80px] lg:text-[112px] lg:leading-[96px] xl:text-[156px] xl:leading-[110px]">
         <PaddingWrapper className="">
           {isToday === 1 ? (
             <div className="flex flex-col justify-start sm:flex-row sm:space-x-2 lg:space-x-3 xl:space-x-4">
@@ -101,17 +101,19 @@ function Countdown() {
             </CountdownOverText>
           )}
 
-          <p className="flex max-w-[70%] justify-start py-[1vh] text-left text-[2.5vh] font-normal leading-[2.5vh] sm:max-w-full sm:items-center sm:justify-center sm:py-[3vh] sm:text-2xl lg:text-3xl xl:text-4xl">
-            Juwenalia Wrocław 2025 już 21 i 22 maja
-          </p>
+          <div className="pt-8 sm:pt-0">
+            <p className="flex max-w-[70%] justify-start py-[1vh] text-left text-lg font-normal leading-[2.5vh] sm:max-w-full sm:items-center sm:justify-center sm:py-[3vh] sm:text-2xl lg:text-3xl xl:text-4xl">
+              Juwenalia Wrocław 2025 już 21 i 22 maja
+            </p>
 
-          <div className="flex origin-left scale-[0.55] transform justify-start text-left sm:origin-center sm:translate-x-0 sm:scale-[0.75] sm:justify-center lg:scale-[0.85] xl:scale-100">
-            <Image
-              src="/wroclawrazem.svg"
-              alt="Ikona logo juwenalia 2025"
-              width={348}
-              height={48}
-            />
+            <div className="flex origin-left scale-[0.75] transform justify-start text-left sm:origin-center sm:translate-x-0 sm:scale-[0.75] sm:justify-center lg:scale-[0.85] xl:scale-100">
+              <Image
+                src="/wroclawrazem.svg"
+                alt="Ikona logo juwenalia 2025"
+                width={348}
+                height={48}
+              />
+            </div>
           </div>
         </PaddingWrapper>
       </div>
