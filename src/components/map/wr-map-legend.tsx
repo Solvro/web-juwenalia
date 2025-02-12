181bd5a784156ccef46445269915a09318caf9e5
Dawid Kowal <dawidkowal04@gmail.com>
2025-02-12 13:34:19 +0100

    chore: disable map dragging on mobile + minor styling changes

--- src/components/map/wr-map-legend.tsx
+++ src/components/map/wr-map-legend.tsx
@@ -3,6 +3,8 @@ import { MapPin } from "lucide-react";
 import type { WrLegendItem } from "@/lib/types";
 import { cn } from "@/lib/utils";
 
+import { PaddingWrapper } from "../padding-wrapper";
+
 interface LegendProps {
   items: WrLegendItem[];
   className?: string;
@@ -10,20 +12,20 @@ interface LegendProps {
 
 export function WrMapLegend({ items, className }: LegendProps) {
   return (
-    <div
+    <PaddingWrapper
       className={cn(
-        "mx-auto grid w-[90%] grid-cols-2 gap-x-5 gap-y-2 sm:grid-cols-[repeat(3,minmax(0,20vw))] md:w-max",
+        "grid w-full grid-cols-1 gap-x-5 gap-y-2 sm:grid-cols-3",
         className,
       )}
     >
       {items.map((item) => (
         <div key={item.name} className="flex items-center">
           <MapPin className="mr-2 h-8 w-8" color={item.color} />
-          <p className="w-[75%] text-wrap text-[3vw] sm:text-base">
+          <p className="line-clamp-2 w-[75%] overflow-hidden text-ellipsis text-wrap text-[3vw] sm:text-base">
             {item.name}
           </p>
         </div>
       ))}
-    </div>
+    </PaddingWrapper>
   );
 }
