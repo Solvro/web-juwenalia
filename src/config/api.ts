8840309755807ce14c4e8048c08e3a7128cdd5b4
Konrad Guzek <konrad@guzek.uk>
2025-02-20 23:47:58 +0100

    feat: feedback form

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,9 @@
 export const API_URL = process.env.API_URL ?? "https://cms.juwenalia.solvro.pl";
+
+export const MUTATION_KEYS = {
+  SEND_FEEDBACK_FORM: "sendFeedbackForm",
+};
+
+export const FEEDBACK_FORM_URL =
+  process.env.FEEDBACK_FORM_URL ??
+  "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdLzzoWZiTN7vDwinKqsU5z4DgMvPhPU0y-yHmaOn9eyDbw_w/formResponse";
