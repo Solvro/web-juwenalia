4b496fb17e7f206d1a4d62961ac4b2c5c7bcb2b2
Konzum59 <jakub.mamut@gmail.com>
2025-02-27 20:18:34 +0100

    feat: adjustments from the review

--- src/components/about-us/organisers.tsx
+++ src/components/about-us/organisers.tsx
@@ -1,29 +1,28 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
 
 import { PersonCardList } from "@/components/about-us/person-card-list";
 import { TabSelectorBar } from "@/components/about-us/tab-selector-bar";
-import { API_URL } from "@/config/api";
 import type { ArrayIndex, Organisation, Person } from "@/lib/types";
+
+import { OrganisationDisplay } from "./organisation-display";
 
 const ROLES = ["Organizatorzy", "Koordynatorzy", "Sztab"] as const;
 
-const isOrganisationArray = (
-  array: Organisation[] | Person[],
-): array is Organisation[] => "url" in array[0];
-
 export function OrganisersList({
-  allOrganisers,
+  organisators,
+  coordinators,
+  staff,
 }: {
-  allOrganisers: (Organisation[] | Person[])[];
+  organisators: Organisation[];
+  coordinators: Person[];
+  staff: Person[];
 }) {
   const [selectedIndex, setSelectedIndex] =
     useState<ArrayIndex<typeof ROLES>>(0);
-  const ORGANISERS: (Organisation[] | Person[])[] = allOrganisers;
-  const section = ORGANISERS[selectedIndex];
+  const section = selectedIndex === 1 ? coordinators : staff;
+
   return (
     <>
       <TabSelectorBar
@@ -31,32 +30,9 @@ export function OrganisersList({
         selectedIdx={selectedIndex}
         setSelectedIdx={setSelectedIndex}
       />
-      {isOrganisationArray(section) ? (
+      {selectedIndex === 0 ? (
         // TODO: Convert into carousel
-        <ul className="grid grid-cols-2 items-center gap-x-4 gap-y-5 sm:gap-x-8 md:grid-cols-3 md:gap-x-12 lg:grid-cols-4 lg:gap-x-16 xl:grid-cols-5 xl:gap-x-20 2xl:gap-x-24">
-          {section.map((organisation) => (
-            <li
-              key={`organiser-${organisation.name}`}
-              className="grid w-full place-items-center"
-            >
-              <Link
-                href={organisation.url}
-                className="group flex gap-2 lg:text-lg xl:text-xl 2xl:text-2xl"
-                target="_blank"
-              >
-                <Image
-                  src={`${API_URL}/assets/${organisation.logo}`}
-                  alt={organisation.name}
-                  width={150}
-                  height={0}
-                  style={{
-                    transform: `scale(${organisation.logoScale?.toString() ?? "1"})`,
-                  }}
-                />
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <OrganisationDisplay forDisplay={organisators} />
       ) : (
         <PersonCardList people={section} />
       )}
