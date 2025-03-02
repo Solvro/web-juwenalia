fa40d4f0b880bd989f26c928b9ffba4b5ec3a600
zeolsem <zsolim@proton.me>
2025-03-02 19:31:49 +0100

    feat: add desktop no-map-info component

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -2,7 +2,61 @@ import { fetchWrMapContent } from "@/lib/wr-map-content";
 
 import { Map } from "./map";
 
-export default async function Page() {
-  const mapLocations = await fetchWrMapContent();
-  return <Map dynamicMapLocations={mapLocations} />;
+import { Button } from "@/components/button";
+import { NoMapInfo } from "@/components/map/no-map-info";
+import { StaticMap } from "@/components/map/static-map";
+
+const DynamicInteractiveMap = dynamic(
+  async () =>
+    import("@/components/map/interactive-map").then(
+      (module_) => module_.InteractiveMap,
+    ),
+  {
+    ssr: false,
+  },
+);
+
+export default function Page() {
+  const [activeStaticMap, setActiveStaticMap] = useState(true);
+  const [activeInteractiveMap, setActiveInteractiveMap] = useState(false);
+
+  function switchMap() {
+    setActiveStaticMap(!activeStaticMap);
+    setActiveInteractiveMap(!activeInteractiveMap);
+  }
+
+  return (
+    <div className="mt-48">
+      <div className="mx-auto mt-10 grid w-4/5 grid-cols-2 gap-10">
+        <Button
+          disabled={activeStaticMap}
+          onClick={() => {
+            switchMap();
+          }}
+          className="text-wrap p-6 text-[3vw] sm:text-base"
+        >
+          Mapa Wydarzenia
+        </Button>
+        <Button
+          disabled={activeInteractiveMap}
+          onClick={() => {
+            switchMap();
+          }}
+          className="text-wrap p-6 text-[3vw] sm:text-base"
+        >
+          Imprezy Towarzyszące
+        </Button>
+      </div>
+      <div>
+        <NoMapInfo />
+        {/* {activeStaticMap ? (
+          <StaticMap />
+        ) : (
+          <Suspense fallback={<div>Loading...</div>}>
+            <DynamicInteractiveMap />
+          </Suspense>
+        )} */}
+      </div>
+    </div>
+  );
 }
