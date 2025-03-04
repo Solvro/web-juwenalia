740b349eb2dab3ad4f9ba47887fb960899c43830
zeolsem <zsolim@proton.me>
2025-03-04 14:42:45 +0100

    fix: swap out map for no-map-info component and fix styles

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,62 +1,140 @@
-import { fetchWrMapContent } from "@/lib/wr-map-content";
+// uncomment when the map is ready to ship
+// "use client";
 
-import { Map } from "./map";
+// import dynamic from "next/dynamic";
+// import { Suspense, useState } from "react";
 
-import { Button } from "@/components/button";
+// import { Button } from "@/components/button";
+// import { StaticMap } from "@/components/map/static-map";
+
+// const DynamicInteractiveMap = dynamic(
+//   async () =>
+//     import("@/components/map/interactive-map").then(
+//       (module_) => module_.InteractiveMap,
+//     ),
+//   {
+//     ssr: false,
+//   },
+// );
+
+// export default function Page() {
+//   const [activeStaticMap, setActiveStaticMap] = useState(true);
+//   const [activeInteractiveMap, setActiveInteractiveMap] = useState(false);
+
+//   function switchMap() {
+//     setActiveStaticMap(!activeStaticMap);
+//     setActiveInteractiveMap(!activeInteractiveMap);
+//   }
+
+//   return (
+//     <div className="mt-48">
+//       <div className="mx-auto mt-10 grid w-4/5 grid-cols-2 gap-10">
+//         <Button
+//           disabled={activeStaticMap}
+//           onClick={() => {
+//             switchMap();
+//           }}
+//           className="text-wrap p-6 text-[3vw] sm:text-base"
+//         >
+//           Mapa Wydarzenia
+//         </Button>
+//         <Button
+//           disabled={activeInteractiveMap}
+//           onClick={() => {
+//             switchMap();
+//           }}
+//           className="text-wrap p-6 text-[3vw] sm:text-base"
+//         >
+//           Imprezy Towarzyszące
+//         </Button>
+//       </div>
+//       <div>
+//         {activeStaticMap ? (
+//           <StaticMap />
+//         ) : (
+//           <Suspense fallback={<div>Loading...</div>}>
+//             <DynamicInteractiveMap />
+//           </Suspense>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// delete once the map is ready to ship
+"use client";
+
 import { NoMapInfo } from "@/components/map/no-map-info";
-import { StaticMap } from "@/components/map/static-map";
 
-const DynamicInteractiveMap = dynamic(
-  async () =>
-    import("@/components/map/interactive-map").then(
-      (module_) => module_.InteractiveMap,
-    ),
-  {
-    ssr: false,
-  },
-);
+// uncomment when the map is ready to ship
+// "use client";
+
+// import dynamic from "next/dynamic";
+// import { Suspense, useState } from "react";
+
+// import { Button } from "@/components/button";
+// import { StaticMap } from "@/components/map/static-map";
+
+// const DynamicInteractiveMap = dynamic(
+//   async () =>
+//     import("@/components/map/interactive-map").then(
+//       (module_) => module_.InteractiveMap,
+//     ),
+//   {
+//     ssr: false,
+//   },
+// );
+
+// export default function Page() {
+//   const [activeStaticMap, setActiveStaticMap] = useState(true);
+//   const [activeInteractiveMap, setActiveInteractiveMap] = useState(false);
+
+//   function switchMap() {
+//     setActiveStaticMap(!activeStaticMap);
+//     setActiveInteractiveMap(!activeInteractiveMap);
+//   }
+
+//   return (
+//     <div className="mt-48">
+//       <div className="mx-auto mt-10 grid w-4/5 grid-cols-2 gap-10">
+//         <Button
+//           disabled={activeStaticMap}
+//           onClick={() => {
+//             switchMap();
+//           }}
+//           className="text-wrap p-6 text-[3vw] sm:text-base"
+//         >
+//           Mapa Wydarzenia
+//         </Button>
+//         <Button
+//           disabled={activeInteractiveMap}
+//           onClick={() => {
+//             switchMap();
+//           }}
+//           className="text-wrap p-6 text-[3vw] sm:text-base"
+//         >
+//           Imprezy Towarzyszące
+//         </Button>
+//       </div>
+//       <div>
+//         {activeStaticMap ? (
+//           <StaticMap />
+//         ) : (
+//           <Suspense fallback={<div>Loading...</div>}>
+//             <DynamicInteractiveMap />
+//           </Suspense>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// delete once the map is ready to ship
 
 export default function Page() {
-  const [activeStaticMap, setActiveStaticMap] = useState(true);
-  const [activeInteractiveMap, setActiveInteractiveMap] = useState(false);
-
-  function switchMap() {
-    setActiveStaticMap(!activeStaticMap);
-    setActiveInteractiveMap(!activeInteractiveMap);
-  }
-
   return (
     <div className="mt-48">
-      <div className="mx-auto mt-10 grid w-4/5 grid-cols-2 gap-10">
-        <Button
-          disabled={activeStaticMap}
-          onClick={() => {
-            switchMap();
-          }}
-          className="text-wrap p-6 text-[3vw] sm:text-base"
-        >
-          Mapa Wydarzenia
-        </Button>
-        <Button
-          disabled={activeInteractiveMap}
-          onClick={() => {
-            switchMap();
-          }}
-          className="text-wrap p-6 text-[3vw] sm:text-base"
-        >
-          Imprezy Towarzyszące
-        </Button>
-      </div>
-      <div>
-        <NoMapInfo />
-        {/* {activeStaticMap ? (
-          <StaticMap />
-        ) : (
-          <Suspense fallback={<div>Loading...</div>}>
-            <DynamicInteractiveMap />
-          </Suspense>
-        )} */}
-      </div>
+      <NoMapInfo />
     </div>
   );
 }
