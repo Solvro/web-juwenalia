0e0dcc5469b4cb815d525b62e10517d860b72929
zeolsem <zsolim@proton.me>
2025-03-27 17:41:12 +0100

    fix: hide map enlargement button on mobile

--- src/components/map/static-map.tsx
+++ src/components/map/static-map.tsx
@@ -4,13 +4,24 @@ import { MAP_ITEMS } from "@/config/legend-items";
 import { cn } from "@/lib/utils";
 
 import { MapEnlargementButton } from "./map-enlargement-button";
+import { useState } from "react";
+import { MapLevelSelector } from "./map-level-selector";
 
 export function StaticMap() {
+  const [currentLevelIndex, setCurrentLevelIndex] = useState<number>(0);
   const currentLevel = MAP_ITEMS[0];
 
   return (
     <div id="map-container">
-      <div className="mt-16 flex w-full flex-row items-center justify-between">
+      <h2 className="mt-16 hidden font-semibold sm:block">Poziomy</h2>
+      <div className="mt-5 hidden w-full sm:grid sm:grid-cols-2 sm:gap-2 xl:grid-cols-4">
+        <MapLevelSelector
+          currentLevelIndex={currentLevelIndex}
+          setCurrentLevelIndex={setCurrentLevelIndex}
+        />
+      </div>
+
+      <div className="mt-16 hidden w-full flex-row items-center justify-between md:block">
         <h2 className="mt-auto font-semibold">Mapa</h2>
         <MapEnlargementButton currentView={currentLevel} />
       </div>
