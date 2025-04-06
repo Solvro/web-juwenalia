4e6c9206b9cc96e172b31611cc21362e564904d1
apoloniaabramowicz <pola.abramowicz@onet.pl>
2025-04-06 20:27:17 +0200

    chore: remove unused things

--- src/app/artists/page.tsx
+++ src/app/artists/page.tsx
@@ -1,5 +1,4 @@
 import { ArtistList } from "@/components/artists/artist-list";
-import { NoArtistInfo } from "@/components/no-artist-info";
 
 export default function ArtistPage() {
   return <ArtistList />;
