f0edc9f83b88aac54a9e7d2ff424780471ef37b5
Konzum59 <jakub.mamut@gmail.com>
2025-04-09 20:24:34 +0200

    feat: add faq wrapper

---
+++ src/app/faq/faq-wrapper.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import type { ClassValue } from "clsx";
+import Image from "next/image";
+
+import { LeafComponent } from "@/components/leaf-component";
+import { PaddingWrapper } from "@/components/padding-wrapper";
+
+export default function FaqWrapper({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: ClassValue;
+}) {
+  return (
+    <section className="relative bottom-[-8rem] mt-48">
+      <h1 className="overflow-wrap w-10/12 pb-3 text-2xl font-semibold leading-10 sm:pb-8 sm:text-3xl sm:font-extrabold md:text-4xl lg:text-5xl xl:text-6xl">
+        <PaddingWrapper>Najczęściej zadawane pytania</PaddingWrapper>
+      </h1>
+
+      <PaddingWrapper className="border-t border-gray-300 pb-40 pt-9 md:pt-16">
+        {children}
+      </PaddingWrapper>
+
+      <LeafComponent className="absolute right-0 top-[-120px] z-[1] flex sm:top-[-130px]">
+        <Image
+          src="/schedule-leaves/leaves-3.svg"
+          alt="dekoracyjne liście"
+          width={80}
+          height={150}
+          className="w-20 scale-x-[-1] scale-y-[-1] sm:w-24 md:w-28 lg:w-32 xl:w-36"
+        />
+      </LeafComponent>
+      <LeafComponent className="absolute bottom-0 right-2/3 z-[1] flex justify-end">
+        <Image
+          src="/no-map-info/branch_br.svg"
+          alt="dekoracyjne liście"
+          width={100}
+          height={80}
+          className="w-32 sm:w-40 md:w-44 lg:w-52 xl:w-56"
+        />
+      </LeafComponent>
+    </section>
+  );
+}
---
+++ src/app/faq/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import FaqWrapper from "./faq-wrapper";
+
+export default function FaqPage() {
+  return (
+    <FaqWrapper>
+      {" "}
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure iusto iste
+      nemo impedit. Nihil nostrum similique culpa illo corporis atque, enim, qui
+      obcaecati numquam consequatur, neque non? Facilis, corrupti quos.{" "}
+      <div> elo żelo </div>
+      <div> elo żelo </div>
+      <div> elo żelo </div>
+      <div> elo żelo </div>
+      <div> elo żelo </div>
+      <div> elo żelo </div>
+      <div> elo żelo </div>
+      <div> elo żelo </div>
+      <div> elo żelo </div>
+    </FaqWrapper>
+  );
+}
