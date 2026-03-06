274e704255984eb595a946b70a591d2d11840bbe
Konrad Guzek <konrad@guzek.uk>
2026-03-06 16:11:25 +0100

    fix(styles): tailwind class warning in dev

--- src/components/arrow-see-more.tsx
+++ src/components/arrow-see-more.tsx
@@ -8,7 +8,7 @@ export function ArrowSeeMore({ text }: { text: string }) {
       <span className="text-lg font-light !normal-case md:text-xl lg:text-2xl">
         {text}
       </span>
-      <ArrowRight className="ease-[cubic-bezier(0.85,0,0.15,1)] mb-1 ml-3 inline-flex -rotate-45 scale-[1.7] stroke-1 transition-transform group-hover:-translate-y-1/4 group-hover:translate-x-1/4 md:scale-[1.8] lg:mb-2 lg:ml-4 lg:scale-[2]" />
+      <ArrowRight className="mb-1 ml-3 inline-flex -rotate-45 scale-[1.7] stroke-1 transition-transform [transition-timing-function:cubic-bezier(0.85,0,0.15,1)] group-hover:-translate-y-1/4 group-hover:translate-x-1/4 md:scale-[1.8] lg:mb-2 lg:ml-4 lg:scale-[2]" />
     </Button>
   );
 }
