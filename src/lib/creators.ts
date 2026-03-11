6f92af5e3bbd8b9015bd4fbc605447b15b614515
Maciej Król <maciekgplay@gmail.com>
2026-03-11 21:11:22 +0100

    refactor: apply copilot review suggestions

--- src/lib/creators.ts
+++ src/lib/creators.ts
@@ -1,6 +1,6 @@
 import { SOLVRO_API_URL } from "@/config/api";
 
-import { FetchError, fetchData } from "./api";
+import { fetchData } from "./api";
 import type { Person } from "./types";
 
 interface JuwenaliaResponseType {
@@ -17,35 +17,30 @@ interface SolvroResponseType {
 }
 
 export async function fetchCreators(): Promise<Person[]> {
-  const response: JuwenaliaResponseType =
-    await fetchData<JuwenaliaResponseType>("items/creators?fields=cms_id");
+  const response = await fetchData<JuwenaliaResponseType>(
+    "items/creators?fields=cms_id",
+  );
 
   const ids = response.data.map((item) => item.cms_id);
 
-  const creatorsResponse = await fetch(
-    `${SOLVRO_API_URL}/items/Team?fields=name,photo&filter[id][_in]=${ids.join(",")}`,
-    {
-      next: { revalidate: 60 },
-    },
+  const parameters = new URLSearchParams({ fields: "name, photo" });
+
+  if (ids.length > 0) {
+    parameters.append("filter[id][_in]", ids.join(","));
+  } else {
+    return [];
+  }
+
+  const creatorsResponse = await fetchData<SolvroResponseType>(
+    `${SOLVRO_API_URL}/items/Team?${parameters.toString()}`,
   );
 
-  let body: SolvroResponseType | null = null;
-
-  try {
-    body = (await creatorsResponse.json()) as SolvroResponseType;
-  } catch (error) {
-    console.warn("Could not parse the response body as JSON", error);
-  }
-  if (!creatorsResponse.ok || body == null) {
-    console.error("Response body:", JSON.stringify(body, null, 2));
-    throw new FetchError(creatorsResponse.statusText);
-  }
-
-  return body.data.map((creator) => {
+  return creatorsResponse.data.map((creator) => {
     return {
       name: creator.name,
       image: creator.photo,
       role: "Twórca strony",
+      isCreator: true,
     };
   });
 }
