396a695352d05d4b13b0c0e9857d47024ed6e35e
Maciej Król <maciekgplay@gmail.com>
2026-03-19 19:10:49 +0100

    refactor: move tab selector headers to separate file

--- src/components/about-us/team.tsx
+++ src/components/about-us/team.tsx
@@ -3,11 +3,10 @@
 import { useState } from "react";
 
 import { TabSelectorBar } from "@/components/about-us/tab-selector-bar";
+import { TEAM_ROLES } from "@/config/tab-selector-headers";
 import type { ArrayIndex, Person } from "@/lib/types";
 
 import { PersonCardList } from "./person-card-list";
-
-const ROLES = ["Sztab", "Twórcy strony"] as const;
 
 export function TeamList({
   staff,
@@ -17,12 +16,12 @@ export function TeamList({
   creators: Person[];
 }) {
   const [selectedIndex, setSelectedIndex] =
-    useState<ArrayIndex<typeof ROLES>>(0);
+    useState<ArrayIndex<typeof TEAM_ROLES>>(0);
 
   return (
     <>
       <TabSelectorBar
-        options={ROLES}
+        options={TEAM_ROLES}
         selectedIdx={selectedIndex}
         setSelectedIdx={setSelectedIndex}
       />
