79d9529adb1680a842bb792473969010ad63e7b8
Filip Winkler <winklaa0@gmail.com>
2026-04-03 09:52:21 +0200

    feat(home-page): adjust tickets, patrons and organizers sections to new design

--- src/components/about-us/partners.tsx
+++ src/components/about-us/partners.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import Image from "next/image";
 import { useState } from "react";
 
 import { TabSelectorBar } from "@/components/about-us/tab-selector-bar";
 import type { ArrayIndex, Organisation } from "@/lib/types";
 
+import { LeafComponent } from "../leaf-component";
 import { OrganisationDisplay } from "./organisation-display";
 
 const PARTNER_TYPES = ["Honorowi", "Medialni"] as const;
@@ -20,7 +22,16 @@ export function PartnersList({
     useState<ArrayIndex<typeof PARTNER_TYPES>>(0);
   const section = selectedIndex === 0 ? mainPartners : mediaPartners;
   return (
-    <>
+    <div className="relative">
+      <LeafComponent className="absolute left-[-20px] top-[-155px] -z-10 sm:top-[-150px] md:left-[-30px] md:top-[-180px] lg:left-[-50px] lg:top-[-215px] xl:left-[-70px] xl:top-[-240px]">
+        <Image
+          src="/bushes/land.svg"
+          alt="About Us"
+          className="w-[100px] sm:w-[90px] md:w-[150px] lg:w-[210px] xl:w-[230px]"
+          width={48}
+          height={20}
+        />
+      </LeafComponent>
       <TabSelectorBar
         options={PARTNER_TYPES}
         selectedIdx={selectedIndex}
@@ -28,6 +39,6 @@ export function PartnersList({
       />
 
       <OrganisationDisplay organisations={section} />
-    </>
+    </div>
   );
 }
