4723bb2217296047596813ee8253d33fc4450aaa
maks1u <smorldesign@gmail.com>
2026-05-18 20:58:15 +0200

    feat: impelment the event map;

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,9 +1,12 @@
-import { NoMapInfo } from "@/components/map/no-map-info";
+import { fetchWrMapContent } from "@/lib/wr-map-content";
 
-export default function Page() {
+import { Map } from "./map";
+
+export default async function Page() {
+  const mapLocations = await fetchWrMapContent();
   return (
     <div className="mt-48">
-      <NoMapInfo />
+      <Map dynamicMapLocations={mapLocations} />
     </div>
   );
 }
